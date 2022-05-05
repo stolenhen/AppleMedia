38f506e2736e25df6900ec57f7034e5767f0845d
stolenhen <stolenhen@gmail.com>
2022-05-05 13:11:59 +0300

    parallax refactoring

--- AppleMedia/Styles/ParallaxView.swift
+++ AppleMedia/Styles/ParallaxView.swift
@@ -11,18 +11,20 @@ struct ParallaxView: View {
     let imagePath: String
     
     var body: some View {
-        GeometryReader {
-            if $0.frame(in: .global).minY > -300 {
+        GeometryReader { proxy in
+            if proxy.frame(in: .global).minY > -300 {
                 WebImageView(imagePath: imagePath.resizedPath(size: 600))
-                    .aspectRatio(contentMode: .fill)
-                    .offset(y: -$0.frame(in: .global).minY)
-                    .frame(
-                        width: Constants.screenWidth,
-                        height: $0.frame(in: .global).minY > 0
-                        ? $0.frame(in: .global).minY + 400
-                        : 400)
+                    .scaledToFill()
+                    .offset(y: -proxy.frame(in: .global).minY)
+                    .frame(width: Constants.screenWidth, height: parallaxHeight(proxy: proxy))
             }
         }
         .frame(height: 300)
+    }
+}
+
+private extension ParallaxView {
+    func parallaxHeight(proxy: GeometryProxy) -> CGFloat {
+        proxy.frame(in: .global).minY > 0 ? proxy.frame(in: .global).minY + 400 : 400
     }
 }
